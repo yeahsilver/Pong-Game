d7eebf5a84e6c8dd51d4ba939bda5a516ccf411c
YeeunHeo <gjdpdms2005@gmail.com>
2020-09-23 00:09:45 +0900

    calculate score

--- Pong-Game/Pong-Game/GameScene.swift
+++ Pong-Game/Pong-Game/GameScene.swift
@@ -14,7 +14,11 @@ class GameScene: SKScene {
     var enemy = SKSpriteNode()
     var main = SKSpriteNode()
     
+    var score = [Int]()
+    
     override func didMove(to view: SKView) {
+        startGame()
+        
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         enemy = self.childNode(withName: "enemy") as! SKSpriteNode
         main = self.childNode(withName: "main") as! SKSpriteNode
@@ -29,6 +33,24 @@ class GameScene: SKScene {
         self.physicsBody = border
     }
 
+    func startGame(){
+        score = [0,0]
+    }
+    
+    func addScore(playerWhoWon: SKSpriteNode){
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerWhoWon == main {
+            score[0] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        } else if playerWhoWon == enemy{
+            score[1] += 1
+            ball.physicsBody?.applyImpulse(CGVector(dx: -20, dy: -20))
+        }
+        print(score)
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         for touch in touches{
             let location = touch.location(in: self)
@@ -47,5 +69,11 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         enemy.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        
+        if ball.position.y <= main.position.y - 70{
+            addScore(playerWhoWon: enemy)
+        } else if ball.position.y >= enemy.position.y + 70 {
+            addScore(playerWhoWon: main)
+        }
     }
 }
